c0eedb3e96e678198ed21c1f8c5acb9970b7144b
Steven <steven@blocked.local>
2025-07-03 15:43:54 +0200

    layout

---
+++ src/php/mailer.php
@@ -0,0 +1,8 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/PHPMailer/src/Exception.php';
+require 'vendor/phpmailer/PHPMailer/src/OAuth/Exception.php';
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/smtp.php';
